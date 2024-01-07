db307ad09b33028e61681b4f43bb324e72457a73
alex_man <alexmnsbs@gmail.com>
2024-01-07 12:52:53 +0300

    feat: is ready

---
+++ core/udemy/java-config/src/main/java/com/udemy/javaconfig/FortuneService.java
@@ -0,0 +1,7 @@
+package com.udemy.javaconfig;
+
+public interface FortuneService {
+
+  public String getFortune();
+
+}
---
+++ core/udemy/java-config/src/main/java/com/udemy/javaconfig/SportConfig.java
@@ -0,0 +1,10 @@
+package com.udemy.javaconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.udemy.javaconfig")
+public class SportConfig {
+
+}
