e0206d59d94310db8ce38eb5f0ed7fe955e12f96
alex_man <alexmnsbs@gmail.com>
2024-01-28 11:28:03 +0300

    feat: add props

--- core/treug/property-source/src/main/java/com/treug/propertysource/one/PersonTest.java
+++ core/treug/property-source/src/main/java/com/treug/propertysource/one/PersonTest.java
@@ -1,4 +1,24 @@
 package com.treug.propertysource.one;
 
+import com.treug.propertysource.config.MyConfig;
+import com.treug.propertysource.model.Person;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class PersonTest {
+
+  public static void main(String[] args) {
+//    ClassPathXmlApplicationContext context =
+//        new ClassPathXmlApplicationContext("appContext.xml");
+    AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(MyConfig.class);
+
+    Person person = context.getBean("myPerson", Person.class);
+
+    System.out.println(person.getSurName());
+    System.out.println(person.getAge());
+
+    context.close();
+
+  }
 }
