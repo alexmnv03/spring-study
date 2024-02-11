d4da73a1c7fbfe9317854284725e329149b0f81c
alex_man <alexmnsbs@gmail.com>
2024-02-11 16:05:19 +0300

    feat: add Qualifier

---
+++ core/treug/annotation-qualifier/src/main/java/com/treug/annotationqualifier/service/QualifierTest.java
@@ -0,0 +1,19 @@
+package com.treug.annotationqualifier.service;
+
+import com.treug.annotationqualifier.model.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class QualifierTest {
+
+  public static void main(String[] args) {
+    ClassPathXmlApplicationContext context =
+        new ClassPathXmlApplicationContext("appContext.xml");
+
+    Person person = context.getBean("personBean", Person.class);
+
+    person.callYourPet();
+
+    context.close();
+
+  }
+}
