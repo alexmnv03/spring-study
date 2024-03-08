0259262c1d993b495bd222d3e72a23d4cfa30462
alex_man <alexmnsbs@gmail.com>
2024-03-08 09:35:49 +0300

    feat: add UniLibraryTwo

---
+++ aop/atreug/aop-Pointcuts/src/main/java/com/treug/aoppointcuts/lib/UniLibraryTwo.java
@@ -0,0 +1,21 @@
+package com.treug.aoppointcuts.lib;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniLibraryTwo {
+
+    public void getBook(String bookName){
+        System.out.println("Мы берем книгу из UniLibrary" + bookName);
+    }
+
+//    public void returnBook(){
+//        System.out.println("Мы возвращаем книгу UniLibrary");
+//    }
+
+    public void getMagazine(){
+        System.out.println("Мы берем журнал из UniLibrary");
+    }
+
+}
+
