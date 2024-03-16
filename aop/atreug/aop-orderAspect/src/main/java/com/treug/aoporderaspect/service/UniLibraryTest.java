f206b524e40e2c57686c70c32414cb863a081671
alex_man <alexmnsbs@gmail.com>
2024-03-16 11:10:54 +0300

    feat: add UniLibraryTest

--- aop/atreug/aop-orderAspect/src/main/java/com/treug/aoporderaspect/service/UniLibraryTest.java
+++ aop/atreug/aop-orderAspect/src/main/java/com/treug/aoporderaspect/service/UniLibraryTest.java
@@ -1,4 +1,21 @@
 package com.treug.aoporderaspect.service;
 
+import com.treug.aoporderaspect.config.MyConfig;
+import com.treug.aoporderaspect.lib.UniLibrary;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class UniLibraryTest {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(MyConfig.class);
+
+        UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
+        uniLibrary.getBook();
+        uniLibrary.returnBook();
+        uniLibrary.getMagazine();
+
+
+        context.close();
+    }
 }
