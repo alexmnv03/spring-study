d0ba8f28a9ef04192c93d182b97360c0c4ed86de
alex_man <alexmnsbs@gmail.com>
2024-03-21 10:29:05 +0300

    feat: change UniversityLoggingAspect

--- aop/atreug/aop-AfterReturning/src/main/java/com/treug/aopafterreturning/aspects/UniversityLoggingAspect.java
+++ aop/atreug/aop-AfterReturning/src/main/java/com/treug/aopafterreturning/aspects/UniversityLoggingAspect.java
@@ -1,5 +1,7 @@
 package com.treug.aopafterreturning.aspects;
 
+import com.treug.aopafterreturning.model.Student;
+import java.util.List;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -14,8 +16,21 @@ public class UniversityLoggingAspect {
         System.out.println("beforeGetStudentsAdvice: Логируем получение списка студентов перед вызовом метода getStudents");
     }
 
-    @AfterReturning("execution(* getStudents())")
-    public void afterReturningGetStudentsAdvice() {
+//    @AfterReturning("execution(* getStudents())")
+//    public void afterReturningGetStudentsAdvice() {
+//        System.out.println("beforeGetStudentsAdvice: Логируем получение списка студентов после вызова метода getStudents");
+//    }
+
+    @AfterReturning(pointcut = "execution(* getStudents())", returning = "list")
+    public void afterReturningGetStudentsAdvice(List<Student> list) {
+        Student first = list.get(0);
+        String name = first.getName();
+        name = "гр. " + name;
+        first.setName(name);
+        double avgGrate = first.getAvgGrade();
+        avgGrate = avgGrate + 0.5;
+        first.setAvgGrade(avgGrate);
         System.out.println("beforeGetStudentsAdvice: Логируем получение списка студентов после вызова метода getStudents");
     }
+
 }
